9b57e933f181ee150f27c1ea4b12f2873b0f6c7b
Luciane Santana de Oliveira <141794038+santanaluciane@users.noreply.github.com>
2023-08-23 10:14:07 -0300

    Delete public directory

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-//CRIAR CONTA
-document.getElementById("create-form").addEventListener("submit", function(e){
-e.preventDefault();
-
-const email = document.getElementById ("email-creat-input");
-const password = document.getElementById ("password-creat-input");
-
-}
